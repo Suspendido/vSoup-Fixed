34709829c40a5bdf4f641abb7d1d37bbd7246109
Comunidad <correotrabajoiWnts@hotmail.com>
2025-12-18 17:52:42 -0600

    fix: spawn its now instant instead of waiting

--- src/main/java/kami/gg/souppvp/command/spawn/SpawnCommand.java
+++ src/main/java/kami/gg/souppvp/command/spawn/SpawnCommand.java
@@ -4,13 +4,10 @@ import kami.gg.souppvp.SoupPvP;
 import kami.gg.souppvp.profile.Profile;
 import kami.gg.souppvp.util.CC;
 import kami.gg.souppvp.util.PlayerUtil;
-import kami.gg.souppvp.util.TimeUtil;
 import kami.gg.souppvp.util.command.Command;
 import kami.gg.souppvp.util.command.CommandManager;
-import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Collections;
 import java.util.List;
@@ -37,11 +34,6 @@ public class SpawnCommand extends Command {
 
         Profile profile = SoupPvP.getInstance().getProfilesHandler().getProfileByUUID(player.getUniqueId());
 
-        if (SoupPvP.getInstance().getSpawnHandler().getCuboid().contains(player)) {
-            player.sendMessage(CC.translate("&cYou can't do this in spawn."));
-            return;
-        }
-
         if (profile.isCombatTagged()) {
             player.sendMessage(CC.translate("&cYou're currently combat-tagged."));
             return;
@@ -57,33 +49,7 @@ public class SpawnCommand extends Command {
             return;
         }
 
-        profile.addSpawnTeleportation();
-
-        new BukkitRunnable() {
-
-            int seconds = 6;
-
-            @Override
-            public void run() {
-                if (!profile.isTeleportingToSpawn()) {
-                    cancel();
-                    return;
-                }
-
-                seconds--;
-
-                if (seconds > 0) {
-                    player.sendMessage(CC.translate("&3Spawn teleportation in &b" + TimeUtil.convertToHhMmSs((long) seconds) + "&3..."));
-                    PlayerUtil.playSound(player, Sound.CHICKEN_EGG_POP);
-                    return;
-                }
-
-                profile.removeSpawnTeleportation();
-                PlayerUtil.resetPlayer(player);
-                cancel();
-            }
-
-        }.runTaskTimerAsynchronously(SoupPvP.getInstance(), 0L, 20L);
+        PlayerUtil.resetPlayer(player);
     }
 
 }
