52d6c1f7d9c473c2ca514f191c3ee19d3fb2d6bf
Comunidad <correotrabajoiWnts@hotmail.com>
2025-12-18 17:47:30 -0600

    feat: added staffhook and logic

---
+++ src/main/java/kami/gg/souppvp/feats/hooks/staff/Staff.java
@@ -0,0 +1,9 @@
+package kami.gg.souppvp.feats.hooks.staff;
+
+import org.bukkit.entity.Player;
+
+public interface Staff {
+
+    boolean isStaffEnabled(Player player);
+    boolean isVanishEnabled(Player player);
+}
