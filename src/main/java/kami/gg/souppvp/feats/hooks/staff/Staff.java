8ee454d28c2ee2b98f3568c9cb95afb991ab3b13
Comunidad <correotrabajoiWnts@hotmail.com>
2025-12-23 16:48:54 -0600

    removal: removal of the staff hook

--- src/main/java/kami/gg/souppvp/feats/hooks/staff/Staff.java
+++
@@ -1,9 +0,0 @@
-package kami.gg.souppvp.feats.hooks.staff;
-
-import org.bukkit.entity.Player;
-
-public interface Staff {
-
-    boolean isStaffEnabled(Player player);
-    boolean isVanishEnabled(Player player);
-}
