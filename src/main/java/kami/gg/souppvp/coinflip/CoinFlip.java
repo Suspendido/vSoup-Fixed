d1bed0626e0688947c78acff600f7046cb724bf2
Comunidad <correotrabajoiWnts@hotmail.com>
2026-01-18 22:05:21 -0600

    changes: all the coinflip changes

--- src/main/java/kami/gg/souppvp/coinflip/CoinFlip.java
+++ src/main/java/kami/gg/souppvp/coinflip/CoinFlip.java
@@ -1,8 +1,7 @@
 package kami.gg.souppvp.coinflip;
 
 import kami.gg.souppvp.SoupPvP;
-import kami.gg.souppvp.coinflip.menu.wager.LoserMenu;
-import kami.gg.souppvp.coinflip.menu.wager.WinnerMenu;
+import kami.gg.souppvp.coinflip.menu.wager.WagerResultMenu;
 import kami.gg.souppvp.profile.Profile;
 import kami.gg.souppvp.util.CC;
 import kami.gg.souppvp.util.TaskUtil;
@@ -11,88 +10,71 @@ import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
-@Getter
-@Setter
+@Getter @Setter
 public class CoinFlip {
 
-    private UUID creator;
-    private Integer amount;
+    private static final Random RANDOM = new Random();
+    private final UUID creator;
+    private final int amount;
     private UUID opponent;
     private UUID winner;
     private UUID loser;
 
-    public CoinFlip(UUID creator, Integer amount){
+    public CoinFlip(UUID creator, int amount) {
         this.creator = creator;
         this.amount = amount;
-        this.opponent = null;
-        this.winner = null;
-        this.loser = null;
+
         SoupPvP.getInstance().getCoinFlipsHandler().getCoinFlips().add(this);
     }
 
-    public void start(){
+    public void start() {
+        SoupPvP plugin = SoupPvP.getInstance();
+        Player creatorPlayer = Bukkit.getPlayer(creator);
+        Player opponentPlayer = Bukkit.getPlayer(opponent);
 
-        Player creator = Bukkit.getPlayer(this.creator);
-        Player opponent = Bukkit.getPlayer(this.opponent);
+        if (creatorPlayer == null || opponentPlayer == null) return;
 
-        List<UUID> creatorAndopponentUUIDs = new ArrayList<>();
-        creatorAndopponentUUIDs.add(this.creator);
-        creatorAndopponentUUIDs.add(this.opponent);
+        boolean creatorWins = RANDOM.nextBoolean();
+        winner = creatorWins ? creator : opponent;
+        loser  = creatorWins ? opponent : creator;
 
-        Random random = new Random();
-        int randomIndex = random.nextInt(creatorAndopponentUUIDs.size());
+        Profile winnerProfile = plugin.getProfilesHandler().getProfileByUUID(winner);
+        Profile loserProfile  = plugin.getProfilesHandler().getProfileByUUID(loser);
 
-        Profile creatorProfile = SoupPvP.getInstance().getProfilesHandler().getProfileByUUID(this.creator);
-        Profile opponentProfile = SoupPvP.getInstance().getProfilesHandler().getProfileByUUID(this.opponent);
+        winnerProfile.setCredits(winnerProfile.getCredits() + amount);
+        winnerProfile.setTotalWagerGames(winnerProfile.getTotalWagerGames() + 1);
+        winnerProfile.setWagersWon(winnerProfile.getWagersWon() + 1);
 
-        if (creatorAndopponentUUIDs.get(randomIndex).equals(this.creator)){
-            this.winner = this.creator;
-            this.loser = this.opponent;
+        loserProfile.setCredits(loserProfile.getCredits() - amount);
+        loserProfile.setTotalWagerGames(loserProfile.getTotalWagerGames() + 1);
+        loserProfile.setWagersLost(loserProfile.getWagersLost() + 1);
 
-            creatorProfile.setCredits(creatorProfile.getCredits() + (amount * 2));
-            creatorProfile.setTotalWagerGames(creatorProfile.getTotalWagerGames() + 1);
-            creatorProfile.setWagersWon(creatorProfile.getWagersWon() + 1);
-
-            opponentProfile.setCredits(opponentProfile.getCredits() - amount);
-            opponentProfile.setTotalWagerGames(opponentProfile.getTotalWagerGames() + 1);
-            opponentProfile.setWagersLost(opponentProfile.getWagersLost() + 1);
-
-        } else {
-            this.winner = this.opponent;
-            this.loser = this.creator;
-
-            opponentProfile.setCredits(opponentProfile.getCredits() + amount);
-            opponentProfile.setTotalWagerGames(opponentProfile.getTotalWagerGames() + 1);
-            opponentProfile.setWagersWon(opponentProfile.getWagersWon() + 1);
-
-            creatorProfile.setTotalWagerGames(creatorProfile.getTotalWagerGames() + 1);
-            creatorProfile.setWagersLost(creatorProfile.getWagersLost() + 1);
-        }
-
-        TaskUtil.runAsync(() -> {
-            new WinnerMenu(this).openMenu(Bukkit.getPlayer(this.getWinner()));
-            new LoserMenu(this).openMenu(Bukkit.getPlayer(this.getLoser()));
+        TaskUtil.run(() -> {
+            new WagerResultMenu(this).openMenu(Bukkit.getPlayer(winner));
+            new WagerResultMenu(this).openMenu(Bukkit.getPlayer(loser));
         });
 
-        if (this.getAmount() > 1000) {
-            Bukkit.getServer().broadcastMessage(CC.translate(""));
-            Bukkit.getServer().broadcastMessage(CC.translate(Bukkit.getPlayer(this.getWinner()).getDisplayName() + " &ebeat &a" + Bukkit.getPlayer(this.getLoser()).getDisplayName() + " &ein a coinflip for &a" + this.getAmount() + " credits&e!"));
-            Bukkit.getServer().broadcastMessage(CC.translate(""));
+        Player nameWinner = Bukkit.getPlayer(winner);
+        Player nameLosser = Bukkit.getPlayer(loser);
+
+        List<String> message = CC.translate(Arrays.asList(
+                "",
+                "&b" + nameWinner.getDisplayName() + " &fbeat &a" + nameLosser.getDisplayName() + " &fin a coinflip for &b" + amount + " credits&f!",
+                ""
+        ));
+
+        for (String s : message) {
+            Bukkit.broadcastMessage(s);
         }
 
-        Bukkit.getPlayer(this.getWinner()).sendMessage(CC.translate("&aYou won the bet and were rewarded! &7(&a" + this.getAmount() + (this.getAmount() != 1 ? " credits&7)" : " credit&7)")));
-        Bukkit.getPlayer(this.getLoser()).sendMessage(CC.translate("&cYou lost the bet and the wager. &7(&a" + this.getAmount() + (this.getAmount() != 1 ? " credits&7)" : " credit&7)")));
+        nameWinner.sendMessage(CC.translate("&a&l+" + amount * 2 + (amount != 1 ? " credits" : " credit")));
+        nameLosser.sendMessage(CC.translate("&c&l-" + amount + (amount != 1 ? " credits" : " credit")));
 
         TaskUtil.runLater(() -> {
-            SoupPvP.getInstance().getCoinFlipsHandler().removeCoinFlip(this);
-            SoupPvP.getInstance().getCoinFlipsHandler().getCoinFlips().remove(this);
+            plugin.getCoinFlipsHandler().removeCoinFlip(this);
+            plugin.getCoinFlipsHandler().getCoinFlips().remove(this);
         }, 10L);
-
     }
-
 }
